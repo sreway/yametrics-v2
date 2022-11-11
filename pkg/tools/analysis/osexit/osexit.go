623cc1aa59b4a94a4fb85ca527033602b40f6285
Andrey Oleynik <a.y.oleynik@gmail.com>
2022-11-11 23:50:11 +0300

    fix check main pkg name

--- pkg/tools/analysis/osexit/osexit.go
+++ pkg/tools/analysis/osexit/osexit.go
@@ -18,31 +18,33 @@ var Analyzer = &analysis.Analyzer{
 
 func run(pass *analysis.Pass) (interface{}, error) {
 	for _, file := range pass.Files {
-		if pass.Pkg.Name() == "main" && len(file.Scope.Objects) == 1 {
-			ast.Inspect(file, func(n ast.Node) bool {
-				if call, ok := n.(*ast.CallExpr); ok {
-					var (
-						id *ast.Ident
-						p  string
-					)
-					switch fun := call.Fun.(type) {
-					case *ast.Ident:
-						id = fun
-					case *ast.SelectorExpr:
-						id = fun.Sel
-						p = fmt.Sprintf("%v", fun.X)
-					}
-
-					if id != nil && id.Name == "Exit" && p == "os" {
-						pass.Report(analysis.Diagnostic{
-							Pos:     call.Lparen,
-							Message: "Found call of os.Exit on main package",
-						})
-					}
-				}
-				return true
-			})
+		if pass.Pkg.Name() != "main" {
+			continue
 		}
+		ast.Inspect(file, func(n ast.Node) bool {
+			if call, ok := n.(*ast.CallExpr); ok {
+				var (
+					id *ast.Ident
+					p  string
+				)
+				switch fun := call.Fun.(type) {
+				case *ast.Ident:
+					id = fun
+				case *ast.SelectorExpr:
+					id = fun.Sel
+					p = fmt.Sprintf("%v", fun.X)
+				}
+
+				if id != nil && id.Name == "Exit" && p == "os" {
+					pass.Report(analysis.Diagnostic{
+						Pos:     call.Lparen,
+						Message: "Found call of os.Exit on main package",
+					})
+				}
+			}
+			return true
+		})
 	}
+
 	return nil, nil
 }
