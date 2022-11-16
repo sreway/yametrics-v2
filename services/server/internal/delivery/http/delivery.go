64feeeb011169526b362cdd22267c951274ec22b
Andrey Oleynik <a.y.oleynik@gmail.com>
2022-11-17 00:56:25 +0300

    add graceful shutdown for http server

--- services/server/internal/delivery/http/delivery.go
+++ services/server/internal/delivery/http/delivery.go
@@ -1,7 +1,13 @@
 package http
 
 import (
+	"context"
+	"errors"
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
+
+	log "github.com/sreway/yametrics-v2/pkg/tools/logger"
 
 	"github.com/sreway/yametrics-v2/services/server/config"
 	"github.com/sreway/yametrics-v2/services/server/internal/usecases"
@@ -22,11 +28,24 @@ func New(uc usecases.Metric, cfg *config.HTTPConfig) *Delivery {
 	return d
 }
 
-func (d *Delivery) Run(cfg *config.HTTPConfig) error {
+func (d *Delivery) Run(ctx context.Context, cfg *config.HTTPConfig) error {
 	httpServer := httpserver.New(d.router, httpserver.Addr(cfg.Address))
+	serverCtx, stopServer := context.WithCancel(context.Background())
+
+	go func() {
+		<-ctx.Done()
+		err := httpServer.Shutdown(serverCtx)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		log.Info("Delivery: graceful shutdown http server")
+		stopServer()
+	}()
+
 	err := httpServer.ListenAndServe()
-	if err != nil {
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
+	<-serverCtx.Done()
 	return nil
 }
